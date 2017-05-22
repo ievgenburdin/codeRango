51619c5e3cd186e62ff9a78297e9e3eae43d47bf
Ievgen Burdin <ievgen.burdin@gmail.com>
2017-05-22 21:26:46 +0300

    made add_page template, change form.py category.html

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -15,20 +15,27 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the URL of page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please insert the URL ofthe page.")
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
 
     class Meta:
         # Provide an association between the ModelForm and a model
         model = Page
         # What fields do we want to include in our form?
         # This way we don't need every field in the model present.
-        # Some fields may allow NULL values, so we may not want to include them.
+        # Some fields may allow NULL values, so we may not want to include them...
         # Here, we are hiding the foreign key.
         # we can either exclude the category field from the form,
         exclude = ('category',)
-        # or specify the fields to include (i.e. not include the category field)
-        # fields = ('title', 'url', 'views')
+        #or specify the fields to include (i.e. not include the category field)
+        #fields = ('title', 'url', 'views')
